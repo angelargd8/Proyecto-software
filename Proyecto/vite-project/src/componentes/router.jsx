25539839c1b0b62439a6a882881a751e2a4a74e2
Diego <dgv3105@gmail.com>
2024-04-18 16:54:09 -0600

    xd

--- Proyecto/vite-project/src/componentes/router.jsx
+++ Proyecto/vite-project/src/componentes/router.jsx
@@ -1,7 +1,7 @@
 import Login from './login.jsx'
 import Home from './home.jsx'
 import SingUp from './signup.jsx'
-import Carrito from './carrito.jsx'
+//import Carrito from './carrito.jsx'
 
 function Router({ruta, setRutaActual}){
     switch(ruta){
@@ -11,8 +11,8 @@ function Router({ruta, setRutaActual}){
             return <Login setRutaActual={setRutaActual}/>
         case "/signup":
                 return <SingUp setRutaActual={setRutaActual}/>
-        case "/carrito":
-                return <Carrito setRutaActual={setRutaActual}/>
+        // case "/carrito":
+        //         return <Carrito setRutaActual={setRutaActual}/>
         case "/":
             return <Home></Home>
         default:
