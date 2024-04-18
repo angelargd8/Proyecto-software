c7752aff94415dd2af42217e4befeac19799d1f0
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-04-18 13:25:43 -0600

    navbar y cambios en la navegacion

--- Proyecto/vite-project/src/App.jsx
+++ Proyecto/vite-project/src/App.jsx
@@ -1,37 +1,16 @@
-import { useState } from 'react'
 import './App.css'
-
 import Router from './componentes/router.jsx'
-
+import Nabvar from './componentes/navbar.jsx'
+import { useState } from 'react'
 //import { useEffect, useState } from 'react'
-
 
 function App() {
   const [rutaActual, setRutaActual] = useState("app")
-  const handleNavigation = (ruta) => {
-    setRutaActual(ruta) //
-  }
- 
-
   return (
     <>
+      <Nabvar setRutaActual={setRutaActual}/>
       <div className="contenedor">
-        <nav >
-          <ul>
-            <li>
-              <a href="/" onClick={(e) => {e.preventDefault(); handleNavigation('/home')}}> Home</a>
-            </li>
-            <li>
-              <a href="/login" onClick={(e) => {e.preventDefault(); handleNavigation('/login')}}>Login</a>
-            </li>
-            
-          </ul>
-        </nav>
-        <div className="contenedor-rutas">
         <Router ruta={rutaActual} setRutaActual={setRutaActual} />
-         
-          
-        </div>
       </div>
     </>
   )
---
+++ Proyecto/vite-project/src/componentes/navbar.jsx
@@ -0,0 +1,68 @@
+import './navbar.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { useEffect } from 'react';
+
+//import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+
+
+function Nabvar({ setRutaActual }){
+
+    useEffect(() => {
+        const handleScroll = () => {
+            var navbar = document.getElementById('navbar');
+            if (window.scrollY > 0) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        // Limpiar el event listener cuando el componente se desmonta
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    
+    
+    return (
+        <>
+        <div className="nabvar-body" id="navbar">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            
+        <a className="navbar-brand" href="/">
+            <img className="logotipo" src="../src/assets/img/logo.png" alt="" width="70" height="80" />
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span style={{width: '30px', height: '3px', backgroundColor: 'black', display: 'block', marginBottom: '5px'}}></span>
+            <span style={{width: '30px', height: '3px', backgroundColor: 'black', display: 'block', marginBottom: '5px'}}></span>
+            <span style={{width: '30px', height: '3px', backgroundColor: 'black', display: 'block'}}></span>
+        </button>
+            <div className="container-fluid">                 
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                   
+
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-text" aria-current="page" href="/" onClick={(e) => {e.preventDefault(); setRutaActual('/home')}}>Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-text" href="/login" onClick={(e) => {e.preventDefault(); setRutaActual('/login')}}>Iniciar Sesion</a>
+                    </li>
+                    
+                </ul>
+                <form className="d-flex buscar" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-success" type="submit">Buscar</button>
+                </form>
+                </div>
+            </div>
+            </nav>
+
+        </div>
+        </>
+      )
+}
+export default Nabvar;
