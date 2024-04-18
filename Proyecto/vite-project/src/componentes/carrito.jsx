286914ebf571c99f747363c15392d50ebb5ba002
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-04-18 15:48:24 -0600

    Carrito de Compras

---
+++ Proyecto/vite-project/src/componentes/carrito.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './carrito.css'
+
+function Carrito(){
+    const [cantidad, setCantidad] = useState(1);
+
+    const sumarCant = () => {
+        setCantidad(cantidad + 1 );
+    };
+
+    const restCant = () => {
+        setCantidad(cantidad -1);
+    };
+
+    
+    return(
+        <>
+            <div className='contenedor'>
+                <div className="up">
+                    <div className='logo'>
+                        <div className="pic"></div>
+                        <div className="regresarbtn"></div>
+                    </div>
+                    
+                    <div id='contTitulo'>
+                        <h1 className='txt'>Orson Library</h1>
+                    </div>
+                </div>
+                <div className='middle'>
+                    <div className="carrito">
+                        <div className='product'>
+                            <div className='vista'></div>
+                            <div className='info'> 
+                                Corona <br/>
+                                SixPack 500ml C/U <br/>
+                                Q 100.00
+                            </div>
+                            <div className='nums'>
+                                <div id='xd'>
+                                    <div className='btn_sumar' onClick={restCant}>-</div>
+                                    <span className='cant'>{cantidad}</span>
+                                    <div className="btn_restar" onClick={sumarCant}>+</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="totales">
+                        <textarea className='codigo' rows={1} placeholder='Codigo Promocional'></textarea>
+                        <div className='rowDatos'>
+                            <div className="columnCampo">
+                                Subtotal <br/>
+                                Envio <br/> <br/>
+                                Total
+                            </div>
+                            <div className="columnNo">
+                                Q 100.00 <br/>
+                                Q 10.00 <br/> <br/>
+                                Q 110.00
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="bottom">
+                    <button className='pagobtn'> Pagar </button>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Carrito
