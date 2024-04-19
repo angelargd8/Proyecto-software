e7c4b8e485560c587d8906b567a13fe29cc10a20
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-04-19 13:05:33 -0600

    XD

--- Proyecto/Servidor/connection.js
+++ Proyecto/Servidor/connection.js
@@ -1,14 +1,14 @@
 const {Pool} = require('pg')
 
 const password1 = "Gerax@2003lm!"
-const password2 = ""
+const password2 = "123456"
 const password3 = ""
 const password4 = ""
 
 const pool = new Pool({
     host:"localhost",
     user: "postgres",
-    password:password1,
+    password:password2,
     database:"ecomers",
     port:"5432"
 })
