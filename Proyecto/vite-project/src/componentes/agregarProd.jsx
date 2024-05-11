f112f1a0fe57d28d63d155d12484c1f444c55198
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-05-10 18:50:25 -0600

    agregar producto

--- Proyecto/vite-project/src/componentes/agregarProd.jsx
+++ Proyecto/vite-project/src/componentes/agregarProd.jsx
@@ -7,7 +7,42 @@ function AgregarProducto(){
             <div className="title">
                 Agregar nuevo producto
             </div>
-            <div className="form">
+            <div className="formulario">
+                <form>
+                    <div className="mb-3">
+                        <label  className="form-label">Nombre del producto</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Descripción</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Precio</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3">
+                        <label  className="form-label">Stock</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Página</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Promociones</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Imagen</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Categoría</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Agregar</button>
+                </form>
                 
             </div>
         </div>
