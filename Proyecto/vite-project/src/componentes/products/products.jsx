effd5feb27317f205369e20b300cf18fc0d1210a
Geraxxx5 <gerax.pineda@gmail.com>
2024-05-19 15:57:54 -0600

    feat (app): add Products page and cardProducts component

---
+++ Proyecto/vite-project/src/componentes/products/products.jsx
@@ -0,0 +1,14 @@
+import CardProduct from "../cardProduct"
+import './products.css'
+
+const Products = () => {
+    return (
+        <div className="container">
+            <CardProduct></CardProduct>
+            <CardProduct></CardProduct>
+            <CardProduct></CardProduct>
+        </div>
+    )
+}
+
+export default Products
