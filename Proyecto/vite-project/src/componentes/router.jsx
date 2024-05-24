f93395e308a95f18881035c665199b20abac9231
faguilarleal <102198566+faguilarleal@users.noreply.github.com>
2024-05-24 16:30:27 -0600

    fix(router)

--- Proyecto/vite-project/src/componentes/router.jsx
+++ Proyecto/vite-project/src/componentes/router.jsx
@@ -2,11 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './log/login.jsx'
 import Home from './home/home.jsx'
 import SingUp from './log/signup.jsx'
-import Detalles from './detalles/detalles.jsx'
 import Carrito from './carrito/carrito.jsx'
 import AgregarProducto from './admin/agregarProd.jsx'
 import EditarProds from './admin/editarProd.jsx';
-import Products from './products/products.jsx';
 import Pago from './carrito/pago.jsx';
 
 function AppRouter(){
@@ -18,7 +16,6 @@ function AppRouter(){
             <Route path="/signup" element={<SingUp />} />
             <Route path="/carrito" element={<Carrito />} />
             <Route path="/pago" element = {<Pago />}/>
-            <Route path="/detalles/:title" element={<Products></Products>} />
             <Route path="/" element={<Home />} />
             <Route path="/agregarProducto" element={<AgregarProducto/>} />
             <Route path="/editarProductos" element={<EditarProds/>} />
