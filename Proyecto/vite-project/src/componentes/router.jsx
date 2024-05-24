c5f86a4b4cb1a259bc7fc03b670ba81bc4bbc325
Geraxxx5 <gerax.pineda@gmail.com>
2024-05-24 16:29:00 -0600

    chor: clean code and change images

--- Proyecto/vite-project/src/componentes/router.jsx
+++ Proyecto/vite-project/src/componentes/router.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './log/login.jsx'
 import Home from './home/home.jsx'
 import SingUp from './log/signup.jsx'
-import Detalles from './detalles/detalles.jsx'
 import Carrito from './carrito/carrito.jsx'
 import AgregarProducto from './admin/agregarProd.jsx'
 import EditarProds from './admin/editarProd.jsx';
