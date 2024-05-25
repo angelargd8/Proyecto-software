9fcef6547c8843ced7de72038476f0408653ddaf
Geraxxx5 <gerax.pineda@gmail.com>
2024-05-24 18:17:49 -0600

    chor: clean code

--- Proyecto/Servidor/connection.js
+++ Proyecto/Servidor/connection.js
@@ -1,11 +1,6 @@
 const {Pool} = require('pg')
 require('dotenv').config()
 
-const password1 = "Gerax@2003lm!"
-const password2 = "123456"
-const password3 = "francis123"
-const password4 = "Basedatos1"
-
 const pool = new Pool({
     host:process.env.DB_HOST,
     user: process.env.DB_USER,
