b577facb92609b10753a9f2b7a113944dd0e4f51
Geraxxx5 <gerax.pineda@gmail.com>
2024-07-30 21:03:00 -0600

    feat(Project): Rename folders and add Dockerfiles

---
+++ Proyecto/BACKEND/api/index.js
@@ -0,0 +1,289 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const {
+  getAllUser,
+  getRol,
+  validateUser,
+  createNewUser,
+  getAllItems,
+  getPage,
+  getPromotions,
+  setNewItem,
+  getOneItem,
+  updateItem,
+  deleteUser,
+  validateEmail,
+  getCategories,
+  getCategory,
+  setCategory,
+  setCategoryItem,
+  getItemsPerCategory,
+} = require("../api/db");
+
+const typeDefs = gql`
+  type Roles {
+    ID: Int!
+    name: String!
+  }
+
+  type Users {
+    email: String!
+    name: String!
+    lastName: String!
+    password: String!
+    rol: Roles
+  }
+
+  type Pages {
+    idPage: Int!
+    name: String!
+    admin: Users
+  }
+
+  type Items {
+    idItems: Int!
+    name: String!
+    quantity: Int
+    price: Float!
+    description: String
+    promotions: [Promotions]
+  }
+
+  type Promotions {
+    idPromotions: Int!
+    percentage: Float
+    state: String!
+    name: String!
+    discount: Float
+  }
+
+  type Categories {
+    idCategory: Int!
+    name: String
+    Page: Pages
+    Items: [Items]
+  }
+
+  type Query {
+    allUsers: [Users]!
+    validateCredentials(email: String!, password: String!): Users
+    getItems: [Items]!
+    getOneItem(idItem: Int!): Items
+    validateEmail(email: String!): Users
+    getCategories: [Categories]!
+    getCategory(idCategory: Int!): Categories
+  }
+
+  type UserValidationResult {
+    status: Boolean!
+    message: String
+    user: Users
+  }
+
+  type StatusItemInsert {
+    status: Boolean!
+    message: String
+    Item: Items
+  }
+
+  type statusCategoryInsert {
+    status: Boolean!
+    message: String
+    Category: Categories
+  }
+
+  type StatusUserDelete {
+    status: Boolean!
+    message: String
+  }
+
+  type StatusInserCategoyItem {
+    status: Boolean!
+    message: String
+  }
+
+  type Mutation {
+    addnewUser(
+      email: String!
+      nombre: String!
+      appelido: String
+      password: String!
+    ): UserValidationResult
+    addNewItem(
+      name: String!
+      quantity: Int
+      price: Float!
+      description: String
+    ): StatusItemInsert
+    updateItem(
+      idItem: Int!
+      name: String!
+      quantity: Int
+      price: Float!
+      description: String
+    ): Items
+    deleteUser(email: String!): StatusUserDelete
+    addNewCategory(name: String!, idPage: Int!): statusCategoryInsert
+    addNewCategoryItem(idCategory: Int!, idItem: Int!): StatusInserCategoyItem
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allUsers: async () => {
+      const users = await getAllUser();
+      return users;
+    },
+    validateCredentials: async (root, args) => {
+      const { email, password } = args;
+      const user = await validateUser(email, password);
+      return user[0];
+    },
+    getItems: async () => {
+      const items = await getAllItems();
+      return items;
+    },
+    getOneItem: async (root, args) => {
+      const { idItem } = args;
+      const item = await getOneItem(idItem);
+      return item[0];
+    },
+    validateEmail: async (root, args) => {
+      const { email } = args;
+      const user = await validateEmail(email);
+      return user[0];
+    },
+    getCategories: async (root, args) => {
+      const categories = await getCategories();
+      return categories;
+    },
+    getCategory: async (root, args) => {
+      const { idCategory } = args;
+      const category = await getCategory(idCategory);
+      return category[0];
+    },
+  },
+  Mutation: {
+    addnewUser: async (root, args) => {
+      const user = { ...args };
+      const response = await validateUser(user.email, user.password);
+      if (response.length < 1) {
+        const result = await createNewUser(user);
+        return {
+          status: true,
+          message: "Usuario creado con exito",
+          user: result,
+        };
+      } else {
+        return {
+          status: false,
+          message: "El usuario ya existe",
+        };
+      }
+    },
+    addNewItem: async (root, args) => {
+      const item = { ...args };
+      const response = await setNewItem(item);
+      return {
+        status: true,
+        message: "Se creo con exito el objeto",
+        Item: response,
+      };
+    },
+    updateItem: async (root, args) => {
+      const newItem = { ...args };
+      const response = await updateItem(newItem);
+      return response;
+    },
+    deleteUser: async (root, args) => {
+      const { email } = args;
+      const response = await deleteUser(email);
+      if (response.rowCount > 0) {
+        return {
+          status: true,
+          message: "Usuario eliminado con exito",
+        };
+      } else {
+        return {
+          status: false,
+          message: "Usuario no encontrado",
+        };
+      }
+    },
+    addNewCategory: async (root, args) => {
+      const { name, idPage } = args;
+      const response = await setCategory(name, idPage);
+      if (response.length > 0) {
+        return {
+          status: true,
+          message: "Se inserto con exito",
+          Category: response[0],
+        };
+      } else {
+        return {
+          status: false,
+          message: "Problema insertando",
+        };
+      }
+    },
+    addNewCategoryItem: async (root, args) => {
+      const { idCategory, idItem } = args;
+      const response = await setCategoryItem(idCategory, idItem);
+      if (response > 0) {
+        return {
+          status: true,
+          message: "Se agrego con exito la relacion",
+        };
+      } else {
+        return {
+          status: false,
+          message: "problema al insertar",
+        };
+      }
+    },
+  },
+  Users: {
+    rol: async (root) => {
+      const rol = await getRol(root.IdRol);
+      return rol[0];
+    },
+  },
+  Items: {
+    promotions: async (root) => {
+      const promotions = await getPromotions(root.idItems);
+      return promotions;
+    },
+  },
+  Categories: {
+    Page: async (root) => {
+      const page = await getPage(root.idPage);
+      return page[0];
+    },
+    Items: async (root) => {
+      const items = await getItemsPerCategory(root.idCategory);
+      return items;
+    },
+  },
+};
+
+/**
+ * Person:{
+        canDrink: (root) => root.age > 18,
+        adress: (root) => {
+            return {
+                street: root.street,
+                city: root.city
+            }
+        }
+    }
+ */
+/**
+ * Si quiero pasar algo es typeDefs: Nombre de mi clase
+ * typedefs y resolvers siempre tienen que ir
+ */
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`server redy at ${url}`));
---
+++ Proyecto/FRONTEND/src/componentes/carrito/pago.jsx
@@ -0,0 +1,93 @@
+import './pago.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { useNavigate } from 'react-router-dom';
+
+
+function Pago(){
+    
+    const navigate = useNavigate();
+
+    
+
+    const handleRegresar = () => {
+        navigate("/carrito");
+    
+    };
+
+    const handleConfirmPago = () => {
+        navigate("/confirmPago")
+    }
+
+    return (
+        <>
+            <div className='contenedor'>
+                <div className="up">
+                    <div className="regresar">
+                        <div className="regresarbtn" onClick={() => handleRegresar()}> &lt; </div>
+                    </div>
+                    <div className='header'>
+                        <div className="tituloDeskPago">
+                        My Cart
+                        </div>
+                        <div className="tituloMovilPago">
+                        Detalles de Pedido
+                        </div>
+                    </div>
+                    <div className="opciones">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span style={{width: '30px', height: '3px', backgroundColor: 'black', display: 'block', marginBottom: '5px'}}></span>
+                            <span style={{width: '30px', height: '3px', backgroundColor: 'black', display: 'block', marginBottom: '5px'}}></span>
+                            <span style={{width: '30px', height: '3px', backgroundColor: 'black', display: 'block'}}></span>
+                        </button>
+                    </div>
+                </div>
+                <div className="MidyBotmPago">
+                    <div className="columnPasosPagar">
+                        <div className="pasos">
+                            <div className="nombrePaso " style={{backgroundColor: 'transparent', color: '#1B4965'}} onClick={() => handleRegresar()}> Direccion de Entrega </div>
+                            <div className="separador">  -------------  </div>
+                            <div className="nombrePaso" > Forma de Pago</div>
+                            <div className="separador">  -------------  </div>
+                            <div className="nombrePaso" style={{backgroundColor: 'transparent', color: '#1B4965'}} onClick={() => handleConfirmPago()}> Ultimo Paso</div>
+                        </div>
+                        <div className="relleno"></div>
+                    </div>
+                    <div className='middle'>
+                        <div className="ParteVenta">
+                            <h5 style={{position: 'absolute', bottom: '80%'}}> Detalles de Entrega</h5>
+                            <div className="ubicacion"> 
+                                <div className="fotoubi"></div>
+                                <div className="nombreUbi">Universidad del Valle de Guatemala</div>
+                            </div>
+                            <div className="receptor">
+                                <div className="fotoRec"></div>
+                                <div className="nombreRec">Kimberly Daniela Morales Ortega</div>
+                            </div>
+                        </div>
+                        <div className="PartePago">
+                            <h5 style={{position: 'absolute', bottom: '52%'}}> Metodos de Pago</h5>
+                            <div className="ubicacion"> 
+                                <div className="fotoTarj"></div>
+                                <div className="nombreUbi">Tarjeta de Credito/Debito</div>
+                            </div>
+                            <div className="receptor">
+                                <div className="fotoEfct"></div>
+                                <div className="nombreRec">Efectivo</div>
+                            </div>                            
+                        </div>
+                        <div className="ParteExtras">
+                            Extras
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <img className="logotipo" src="../src/assets/img/logo.png" style={{ width: 60, height: 70, marginLeft: '1%'}}  />
+                        Picolin 
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Pago;
