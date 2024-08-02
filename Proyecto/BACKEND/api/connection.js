29381478d6b25eb58276801c633cd878c3dc1b3c
Geraxxx5 <gerax.pineda@gmail.com>
2024-08-02 16:34:30 -0600

    update packega.json file

--- Proyecto/BACKEND/api/connection.js
+++ Proyecto/BACKEND/api/connection.js
@@ -1,8 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-console.warn("AYUDA", process.env.DB_HOST);
-
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
