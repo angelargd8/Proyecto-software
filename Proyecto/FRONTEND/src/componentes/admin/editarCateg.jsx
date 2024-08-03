ac622afb4c4031d68e66cefe801b1f3dbf005485
Geraxxx5 <gerax.pineda@gmail.com>
2024-08-02 19:16:01 -0600

    feat: add enviroment variables

--- Proyecto/FRONTEND/src/componentes/admin/editarCateg.jsx
+++ Proyecto/FRONTEND/src/componentes/admin/editarCateg.jsx
@@ -1,7 +1,7 @@
 import Card from "../home/card";
-import './editarCateg.css';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import "./editarCateg.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 function EditCateg() {
@@ -9,39 +9,61 @@ function EditCateg() {
   const location = useLocation();
   const navigate = useNavigate();
 
-  // Ir al apartado de edición de productos 
+  // Ir al apartado de edición de productos
   const info = (title, cardInfo) => {
     navigate(`/detalles/${title}`, { state: { cardInfo } });
   };
 
   // Eliminar categoría
   const eliminarCategoria = (id) => {
-    const newCards = listadoCards.filter(card => card.id !== id);
+    const newCards = listadoCards.filter((card) => card.id !== id);
     setCards(newCards);
   };
 
   // Agregar categoría
   const agregarCategoria = () => {
-    navigate('/agregarCategoria');
+    navigate("/agregarCategoria");
   };
 
   // Cargar las cards
   const cargarCards = () => {
     const searchParams = new URLSearchParams(location.search);
-    const searchItem = searchParams.get('search') || '';
+    const searchItem = searchParams.get("search") || "";
 
     const cards = [
-      { id: 1, title: "Brillantina", imagen: "../src/assets/img/Brillantina-surtida.jpg" },
-      { id: 2, title: "Ojos", imagen: "../src/assets/img/Ojos/Ojitos NO 1.jpg" },
-      { id: 3, title: "Añelina", imagen: "../src/assets/img/Colorante/AÑELINA.jpg" },
-      { id: 4, title: "Pulseras", imagen: "../src/assets/img/Pulseras/PULSERA PASTEL.jpg" },
-      { id: 5, title: "Flores", imagen: "../src/assets/img/Flores/FLOR GRANDE.jpg" },
+      {
+        id: 1,
+        title: "Brillantina",
+        imagen: "../src/assets/img/Brillantina-surtida.jpg",
+      },
+      {
+        id: 2,
+        title: "Ojos",
+        imagen: "../src/assets/img/Ojos/OJITOS NO 1.jpg",
+      },
+      {
+        id: 3,
+        title: "Añelina",
+        imagen: "../src/assets/img/Colorante/AÑELINA.jpg",
+      },
+      {
+        id: 4,
+        title: "Pulseras",
+        imagen: "../src/assets/img/Pulseras/PULSERA PASTEL.jpg",
+      },
+      {
+        id: 5,
+        title: "Flores",
+        imagen: "../src/assets/img/Flores/FLOR GRANDE.jpg",
+      },
     ];
 
-    if (searchItem.trim() === '') {
+    if (searchItem.trim() === "") {
       setCards(cards);
     } else {
-      const filteredCards = cards.filter(card => card.title.toLowerCase().includes(searchItem.toLowerCase()));
+      const filteredCards = cards.filter((card) =>
+        card.title.toLowerCase().includes(searchItem.toLowerCase())
+      );
       setCards(filteredCards);
     }
   };
@@ -52,32 +74,37 @@ function EditCateg() {
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    if (searchParams.get('search')) {
-      searchParams.delete('search');
+    if (searchParams.get("search")) {
+      searchParams.delete("search");
       navigate(`/home?${searchParams.toString()}`, { replace: true });
     }
   }, []);
 
   return (
     <div className="container-home">
-        <div className="header">
-            <h1>Editar Categorías</h1>
-            <button className="agregar-categoria-button" onClick={agregarCategoria}>
-                Agregar Categoría
+      <div className="header">
+        <h1>Editar Categorías</h1>
+        <button className="agregar-categoria-button" onClick={agregarCategoria}>
+          Agregar Categoría
+        </button>
+      </div>
+      <div id="contenido-cartas">
+        {listadoCards.map((elemento) => (
+          <div key={elemento.id} className="category-card">
+            <Card
+              title={elemento.title}
+              imagen={elemento.imagen}
+              onClick={() => info(elemento.title, elemento)}
+            />
+            <button
+              className="eliminar-button"
+              onClick={() => eliminarCategoria(elemento.id)}
+            >
+              Eliminar Categoría
             </button>
-        </div>
-        <div id="contenido-cartas">
-          {listadoCards.map((elemento) => (
-            <div key={elemento.id} className="category-card">
-              <Card title={elemento.title}
-                imagen={elemento.imagen}
-                onClick={() => info(elemento.title, elemento)} />
-              <button className="eliminar-button" onClick={() => eliminarCategoria(elemento.id)}>
-                Eliminar Categoría
-              </button>
-            </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
