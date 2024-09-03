5aef873c8538a59098a73d9b7331e306e351c7b4
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-09-03 07:52:54 -0600

    fix(): Only show whatsapp btn where necessary

--- Proyecto/FRONTEND/src/componentes/floatingButton/floatingButton.jsx
+++ Proyecto/FRONTEND/src/componentes/floatingButton/floatingButton.jsx
@@ -1,26 +1,38 @@
-import './floatingButton.css';
+import "./floatingButton.css";
+import { useLocation } from "react-router-dom";
+
 function FloatingButton() {
+  const location = useLocation();
+  const handleClick = () => {
+    window.open("https://wa.me/50237067222", "_blank");
+  };
 
-   const handleClick = () =>{
-        window.open('https://wa.me/50237067222', '_blank');
-   } 
+  if (location.pathname !== "/home") {
+    if (location.pathname !== "/contact") {
+      return;
+    }
+  }
 
-    return (
+  return (
     <div className="floatingButton-body">
-        
-        <div className="floatingButton">
-        <button type="submit" onClick={handleClick} id='boton-whatsapp' className="imagen-btn-whatsapp">
-            <div className="imagen">
-                <img className="imagen-whatsapp" src="./src/assets/img/whatsapp.png" alt="whatsapp" />
-            </div>
-            
+      <div className="floatingButton">
+        <button
+          type="submit"
+          onClick={handleClick}
+          id="boton-whatsapp"
+          className="imagen-btn-whatsapp"
+        >
+          <div className="imagen">
+            <img
+              className="imagen-whatsapp"
+              src="./src/assets/img/whatsapp.png"
+              alt="whatsapp"
+            />
+          </div>
         </button>
+      </div>
     </div>
-
-    </div>
-
-    );
-
+  );
 }
 
 export default FloatingButton;
