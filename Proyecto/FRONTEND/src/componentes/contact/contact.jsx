d63b8dc5fcb1a31f77414475fcf411514934f6ca
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-09-03 07:52:22 -0600

    chore(): Clean code

--- Proyecto/FRONTEND/src/componentes/contact/contact.jsx
+++ Proyecto/FRONTEND/src/componentes/contact/contact.jsx
@@ -1,30 +1,28 @@
-import './contact.css';
+import "./contact.css";
 
-function Contact(){
-    return(
-        <>
-        <div className="contact body">
-            
-            <div className="info"> 
-                <h1 className="main-text">Contáctanos</h1>
-                <h1 className="second-text">¡Estamos para servirte!</h1>
-            </div>
-            
-            <div className="cuadro">
-                <div className="container-image">
-                    <div className="imagen"></div>
-                </div>
-                <div className="container-info">
-                    <div className="contact-info">
-                        <h1 className="text">WhatsApp: 37067222</h1>
-                        <h1 className="text">Correo: picolinescolor@gmail.com </h1>
-                    </div>
-                </div>
-            </div>
+function Contact() {
+  return (
+    <>
+      <div className="contact body">
+        <div className="info">
+          <h1 className="main-text">Contáctanos</h1>
+          <h1 className="second-text">¡Estamos para servirte!</h1>
         </div>
-        </>
-    )
 
+        <div className="cuadro">
+          <div className="container-image">
+            <div className="imagen"></div>
+          </div>
+          <div className="container-info">
+            <div className="contact-info">
+              <h1 className="text">WhatsApp: 37067222</h1>
+              <h1 className="text">Correo: picolinescolor@gmail.com </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
