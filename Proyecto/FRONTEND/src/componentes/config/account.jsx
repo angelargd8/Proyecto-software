88b45accc64f31bcff072dbd01b807c879974c46
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-09-22 17:24:57 -0600

    style(account & configuration)

--- Proyecto/FRONTEND/src/componentes/config/account.jsx
+++ Proyecto/FRONTEND/src/componentes/config/account.jsx
@@ -18,23 +18,33 @@ function Account() {
 
 
     return (
-        <div>
+        <div className="AccountContainer">
             {/* si el active form no es change password solo va a mostrar lo de informacion de la cuenta*/
             activeForm !== 'changePassword' && (
                 <button onClick={handleShowInfoForm}>
+                    <span className="texto-principal">
                     {
                     activeForm === 'info' ? 'Regresar' : 'Información de la cuenta'
                     }
+                    </span>
+                    <span className="descripcion">
+                        {activeForm !== 'info' && 'Ve o cambia la información de tu cuenta, como tu nombre y direccion correo electrónico'}
+                    </span>
                 </button>
             )}
             {activeForm === 'info' && <InfoAccountForm />}
 
-            {/* si el active form no es infosolo va a mostrar lo de cambiar contraseña*/
+            {/* si el active form no es info solo va a mostrar lo de cambiar contraseña*/
             activeForm !== 'info' && (
                 <button onClick={handleChangePasswordForm}>
+                    <span className="texto-principal">
                     {
                     activeForm === 'changePassword' ? 'Regresar' : 'Cambia la contraseña'
                     }
+                    </span>
+                    <span className="descripcion">
+                        {activeForm !== 'changePassword' && 'Cambia tu contraseña actual por una nueva'}
+                    </span>
                 </button>
             )}
             {activeForm === 'changePassword' && <ChangePasswordForm />}
