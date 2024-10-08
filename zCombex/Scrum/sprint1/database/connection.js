36c3c0ec0f763e7d847945aeb834f770f7f7911f
Angela Rosana Garcia <angelargd8@outlook.com>
2024-10-08 17:00:36 -0600

    remove(combex files)

--- zCombex/Scrum/sprint1/database/connection.js
+++
@@ -1,9 +0,0 @@
-const oracledb = require('oracledb');
-
-const pool = oracledb.createPool({
-    user: 'gerax',
-    password: 'admin',
-    connectString: 'localhost/xepdb1'
-})
-
-module.exports = pool
