0b215e6d67e0838af6b9afc168ec26990b368954
Angela Rosana Garcia <angelargd8@outlook.com>
2024-10-14 00:08:18 -0600

    refactor(css)

---
+++ Proyecto/FRONTEND/src/componentes/footer/footer.jsx
@@ -0,0 +1,15 @@
+import './footer.css';
+import React from 'react'
+
+function Footer(){
+    return(
+        <div className="Container-footer">
+            <div className="footer-content">
+                <h1 className="footer-text">Picolines Color</h1>
+                <h1 className="footer-text">¡Estamos para servirte!</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
