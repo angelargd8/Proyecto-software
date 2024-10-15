b1b730c2dee4511a9a3562cd90c0892f11870bbd
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2024-10-15 17:12:12 -0600

    chore(): clean code

--- Proyecto/FRONTEND/src/App.jsx
+++ Proyecto/FRONTEND/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css';
-import AppRouter from './componentes/router.jsx';
-import Nabvar from './componentes/navbar/navbar.jsx';
-import { BrowserRouter as Router, useLocation } from 'react-router-dom';
-import { CarritoProvider } from './componentes/carrito/carritoContext.jsx';
-import FloatingButton from './componentes/floatingButton/floatingButton.jsx';
-import Footer from './componentes/footer/footer.jsx';
+import "./App.css";
+import AppRouter from "./componentes/router.jsx";
+import Nabvar from "./componentes/navbar/navbar.jsx";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
+import { CarritoProvider } from "./componentes/carrito/carritoContext.jsx";
+import FloatingButton from "./componentes/floatingButton/floatingButton.jsx";
+import Footer from "./componentes/footer/footer.jsx";
 
 function App() {
   return (
@@ -20,22 +20,32 @@ function App() {
 
 function AppContent() {
   const location = useLocation();
-  const isPageWithNoNav = location.pathname === "/login" | location.pathname === "/signup" | location.pathname === "/carrito" ;
-  const isPageWithNoFooter = location.pathname === "/login" | location.pathname === "/signup" | location.pathname === "/carrito" | location.pathname === "/configuracion" | location.pathname === "/pago" | location.pathname === "/resumen" | location.pathname === "/editarCategorias" | location.pathname === "/detalles" | location.pathname === "/agregarCategoria";
+  const isPageWithNoNav =
+    (location.pathname === "/login") |
+    (location.pathname === "/signup") |
+    (location.pathname === "/carrito") |
+    (location.pathname === "/pago");
+
+  const isPageWithNoFooter =
+    (location.pathname === "/login") |
+    (location.pathname === "/signup") |
+    (location.pathname === "/carrito") |
+    (location.pathname === "/configuracion") |
+    (location.pathname === "/pago") |
+    (location.pathname === "/resumen") |
+    (location.pathname === "/editarCategorias") |
+    (location.pathname === "/detalles") |
+    (location.pathname === "/agregarCategoria");
   return (
     <>
       {!isPageWithNoNav && <Nabvar />}
       <FloatingButton />
-      <div className={`contenedor ${isPageWithNoNav ? 'no-margin' : ''}`}>
+      <div className={`contenedor ${isPageWithNoNav ? "no-margin" : ""}`}>
         <AppRouter />
       </div>
-      {!isPageWithNoFooter  && <Footer />}
-
-     
+      {!isPageWithNoFooter && <Footer />}
     </>
   );
 }
 
 export default App;
-
-
