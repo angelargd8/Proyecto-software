cdab46d66f0242c1f42fd5168e047ba6bee85e93
Angela Rosana Garcia <angelargd8@outlook.com>
2024-10-15 18:28:02 -0600

    Update footer.jsx

--- Proyecto/FRONTEND/src/componentes/footer/footer.jsx
+++ Proyecto/FRONTEND/src/componentes/footer/footer.jsx
@@ -13,11 +13,11 @@ function Footer(){
                         <p className="footer-text">Teléfono: 1234567890</p>
                         <p className="footer-text">Correo: picolinescolor@gmail.com</p>
                     </div>
-                    <div className="ColumnContainer">
+                    {/* <div className="ColumnContainer">
                         <h3 className="footer-text">Más informacion</h3>
                         <p className="footer-text">Home</p>
                         <p className="footer-text"></p>
-                    </div>
+                    </div> */}
                 </div>
                 <hr></hr>
                 <div className="footer-content">
