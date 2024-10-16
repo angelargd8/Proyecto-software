3671bcfb04b70dd466a7459c294b5065bab916a5
Geraxxx5 <gerax.pineda@gmail.com>
2024-10-15 18:40:22 -0600

    fix: display categori products

--- Proyecto/FRONTEND/src/componentes/products/products.jsx
+++ Proyecto/FRONTEND/src/componentes/products/products.jsx
@@ -55,151 +55,18 @@ const Products = () => {
   const { detail } = useParams();
   return (
     <div className="container">
-      {detail == "Brillantina" && (
-        // <>
-        //   <CardProduct
-        //     id={"1"}
-        //     title={"Carton de brillantina Tierra"}
-        //     description={"Cartones de brillantina de 40 sobre estilo tierra"}
-        //     image={"../src/assets/img/Brillantina-tierra.jpg"}
-        //     precios={[
-        //       ["Unidad", 12, 1],
-        //       ["Docena", 10, 12],
-        //       ["millar", 9, 15],
-        //     ]}
-        //   ></CardProduct>
-        //   <CardProduct
-        //     id={"2"}
-        //     title={"Carton de brillantina Surtida"}
-        //     description={"Cartones de brillantina de 40 sobre estilo surtida"}
-        //     image={"../src/assets/img/Brillantina-surtida.jpg"}
-        //     precios={[
-        //       ["Unidad", 12, 1],
-        //       ["Docena", 10, 12],
-        //     ]}
-        //   ></CardProduct>
-        //   <CardProduct
-        //     id={"3"}
-        //     title={"Carton de brillantina Tornasol"}
-        //     description={"Cartones de brillantina de 40 sobre estilo tornasol"}
-        //     image={"../src/assets/img/Brillantina-tornasol.jpg"}
-        //     precios={[
-        //       ["Unidad", 12, 1],
-        //       ["Docena", 10, 12],
-        //     ]}
-        //   ></CardProduct>
-        // </>
-        <>
-          {products.map((product, index) => {
-            return (
-              <CardProduct
-                key={index}
-                id={index}
-                title={product.name}
-                description={product.description}
-                image={product.image}
-                precios={product.prices}
-              />
-            );
-          })}
-        </>
-      )}
-      {/* {detail == "Ojos" && (
-        <>
+      {products.map((product, index) => {
+        return (
           <CardProduct
-            id={"1"}
-            title={"Carton ojitos pequeños"}
-            description={"Cartones de ojos de 20 sobres tamaño pequeño"}
-            image={"../src/assets/img/Ojos/OJITOS NO 1.jpg"}
-            precios={[
-              ["Unidad", 12, 12],
-              ["Docena", 10],
-            ]}
-          ></CardProduct>
-          <CardProduct
-            id={"2"}
-            title={"Carton ojitos grandes"}
-            description={"Cartones de ojos de 20 sobres tamaño grande"}
-            image={"../src/assets/img/Ojos/OJITOS 2.jpg"}
-            precios={[
-              ["Unidad", 12],
-              ["Docena", 10],
-            ]}
-          ></CardProduct>
-        </>
-      )}
-      {detail == "Añelina" && (
-        <>
-          <CardProduct
-            id={"1"}
-            title={"Carton de añelina"}
-            description={"Cartones de añelina de 40 sobres de colores"}
-            image={"../src/assets/img/Colorante/AÑELINA.jpg"}
-            precios={[
-              ["Unidad", 50],
-              ["Docena", 45],
-            ]}
-          ></CardProduct>
-          <CardProduct
-            id={"2"}
-            title={"Carton de colorante vegetal"}
-            description={"Carton de colorante vegetal de 40 sobres de colores"}
-            image={"../src/assets/img/Colorante/COLORANTE VEGETAL.jpg"}
-            precios={[
-              ["Unidad", 70],
-              ["Docena", 65],
-            ]}
-          ></CardProduct>
-        </>
-      )}
-      {detail == "Pulseras" && (
-        <>
-          <CardProduct
-            id={"1"}
-            title={"Carton de añelina"}
-            description={"Cartones de añelina de 40 sobres de colores"}
-            image={"../src/assets/img/Colorante/AÑELINA.jpg"}
-            precios={[
-              ["Unidad", 50],
-              ["Docena", 45],
-            ]}
-          ></CardProduct>
-          <CardProduct
-            id={"2"}
-            title={"Carton de colorante vegetal"}
-            description={"Carton de colorante vegetal de 40 sobres de colores"}
-            image={"../src/assets/img/Colorante/COLORANTE VEGETAL.jpg"}
-            precios={[
-              ["Unidad", 70],
-              ["Docena", 65],
-            ]}
-          ></CardProduct>
-        </>
-      )}
-      {detail == "Flores" && (
-        <>
-          <CardProduct
-            id={"1"}
-            title={"Carton de añelina"}
-            description={"Cartones de añelina de 40 sobres de colores"}
-            image={"../src/assets/img/Colorante/AÑELINA.jpg"}
-            precios={[
-              ["Unidad", 50],
-              ["Docena", 45],
-            ]}
-          ></CardProduct>
-          <CardProduct
-            id={"2"}
-            title={"Carton de colorante vegetal"}
-            description={"Carton de colorante vegetal de 40 sobres de colores"}
-            image={"../src/assets/img/Colorante/COLORANTE VEGETAL.jpg"}
-            precios={[
-              ["Unidad", 70],
-              ["Docena", 65],
-            ]}
-          ></CardProduct>
-        </>
-      )} */}
+            key={index}
+            id={index}
+            title={product.name}
+            description={product.description}
+            image={product.image}
+            precios={product.prices}
+          />
+        );
+      })}
     </div>
   );
 };
