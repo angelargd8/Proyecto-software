0c0f01dd44f49a5254d9af298672faa488835162
Angela Rosana Garcia <angelargd8@outlook.com>
2024-10-21 22:08:21 -0600

    fix(css): contact

--- Proyecto/FRONTEND/src/componentes/contact/contact.jsx
+++ Proyecto/FRONTEND/src/componentes/contact/contact.jsx
@@ -6,34 +6,36 @@ function Contact() {
   return (
     <>
       <div className="contact body">
-        <div className="info">
-          <h1 className="main-text">Contáctanos</h1>
-          <h1 className="second-text">¡Estamos para servirte!</h1>
-        </div>
-        <hr />
-        <div className="cuadro">
-          <div className="container-image">
-            <div className="imagen"
-              style={{
-                backgroundImage: `url(${logo})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                width: '100%',
-                height: '150px',
-              }}>
-            </div>
+        <div className="ContainerContac">
+          <div className="info">
+            <h1 className="main-text"> Contáctanos </h1>
+            <h1 className="second-text">¡Estamos para servirte!</h1>
           </div>
-          <div className="container-info">
-            <div className="contact-info">
-              <h1 className="text">
-                <LogoWhatsapp color="#cea44a" />
-                 :3706-7222
-              </h1>
-              <h1 className="text">
-                <MailOutline color="#cea44a" />
-                 :picolinescolor@gmail.com
-              </h1>
+          <hr />
+          <div className="cuadro">
+            <div className="container-image">
+              <div className="imagen"
+                style={{
+                  backgroundImage: `url(${logo})`,
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  width: '100%',
+                  height: '315px',
+                }}>
+              </div>
+            </div>
+            <div className="container-info">
+              <div className="contact-info">
+                <h1 className="text">
+                  <LogoWhatsapp color="#cea44a" />
+                  :3706-7222
+                </h1>
+                <h1 className="text">
+                  <MailOutline color="#cea44a" />
+                  :picolinescolor@gmail.com
+                </h1>
+              </div>
             </div>
           </div>
         </div>
