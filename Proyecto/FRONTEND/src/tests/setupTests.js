4dbcfc982eb6950e066b556ad9ff258882dcd1f7
Angela Rosana Garcia <angelargd8@outlook.com>
2024-11-01 19:52:15 -0600

    styled-components

--- Proyecto/FRONTEND/src/tests/setupTests.js
+++ Proyecto/FRONTEND/src/tests/setupTests.js
@@ -2,5 +2,6 @@ import '@testing-library/jest-dom';
 import 'identity-obj-proxy';
 import fetchMock from 'jest-fetch-mock';
 import $ from 'jquery'; 
+import 'styled-components';
 
 fetchMock.enableMocks();
