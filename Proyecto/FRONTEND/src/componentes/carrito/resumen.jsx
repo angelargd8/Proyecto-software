d99446d4c23bc245ad1c24e7cd05b57e7f427bbe
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2024-11-12 18:08:04 -0600

    chore(): cleaned code

--- Proyecto/FRONTEND/src/componentes/carrito/resumen.jsx
+++ Proyecto/FRONTEND/src/componentes/carrito/resumen.jsx
@@ -89,18 +89,6 @@ const Resumen = () => {
   );
 };
 
-const mobileStyles = {
-  display: "flex",
-  flexDirection: "column",
-  position: "absolute",
-  top: 0,
-  left: 0,
-  bottom: 0,
-  right: 0,
-  alignItems: "center",
-  justifyContent: "center",
-};
-
 const styles = {
   contenedorGeneral: {
     display: "flex",
